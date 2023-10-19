62c211e3e748f9485c185d9a11b64e3519252c2f
minhtamphan373 <minhtamphan373@gmail.com>
2023-10-19 08:49:48 +0700

    update routes/path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,18 @@ import { LoginUserComponent } from './pages/login-user/login-user.component';
 import { RegisterUserComponent } from './pages/register-user/register-user.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
-import { LibraryComponent } from './pages/library/library.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {
-    path: 'home',
-    component: HomeComponent,
-    pathMatch: 'full'
-  },
-  {path: 'login-user', component: LoginUserComponent},
-  {path: 'register-user', component: RegisterUserComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'library', component: LibraryComponent},
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  //   pathMatch: 'full'
+  // },
+  // {path: 'login-user', component: LoginUserComponent},
+  // {path: 'register-user', component: RegisterUserComponent},
+  // {path: 'contact', component: ContactComponent},
+  // {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
