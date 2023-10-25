c5f4a43f9ccef1eb84707ffe875912f8374114a7
minhtamphan373 <minhtamphan373@gmail.com>
2023-10-25 14:21:49 +0700

    update frontend

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Sach } from 'src/app/models/Sach';
+import { SachService } from 'src/app/services/sach.service';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.css']
 })
-export class LibraryComponent {
+export class LibraryComponent{
+  saches: Sach[] = [];
+  sachToEdit?: Sach;
 
+  constructor(private sachService: SachService) {
+  
+  }
+
+  ngOnInit(): void {
+    this.sachService.getSaches()
+    .subscribe((result: Sach[]) => (this.saches = result));
+  }
+
+  updateSachList(saches: Sach[]){
+    this.saches = saches;
+  }
+
+  initNewSach(){
+    this.sachToEdit = new Sach();
+  }
+
+  editSach(sach: Sach){
+    this.sachToEdit = sach;
+  }
+
+  // updateSach(sach: Sach){}
 }
+
